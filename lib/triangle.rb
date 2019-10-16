b446300d17e914ada2779797fef37ee5f0adc648
Jared S <jaredjames2020@gmail.com>
2019-10-16 17:50:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
       #   raise TriangleError
       # rescue TriangleError => error
       #     puts error.message
-    end
+    #end
   end
     
   # class TriangleError < StandardError
