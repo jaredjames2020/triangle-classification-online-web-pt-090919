29ca182e51c3fa9ddc94466b37b3830a73cb8c21
Jared S <jaredjames2020@gmail.com>
2019-10-16 17:53:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,16 +1,16 @@
 class Triangle
   # write code here
   
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
-  end
-  
   class TriangleError < StandardError
     def message
       "Invalid Triangle!"
     end
+  end
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
   
   def kind
