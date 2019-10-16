16e281d5b12e7e9f18f45a0fe5d59e03f35f8e71
Jared S <jaredjames2020@gmail.com>
2019-10-16 18:37:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,19 +18,19 @@ class Triangle
   
   def kind
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
-      begin
+      # begin
           raise TriangleError
-        rescue TriangleError => error
-          puts error.message
-      end
+        # rescue TriangleError => error
+          # puts error.message
+      # end
     end
     
     if (@side1 + @side2) > @side3 || (@side2 + @side3) > @side1 || (@side3 + @side1) > @side2
-      begin
+      # begin
         raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+      # rescue TriangleError => error
+          # puts error.message
+      # end
     end  
     
     if @side1 == @side2 && @side2 == @side3
