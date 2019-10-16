d1a500aee017cabb737f01d2b3968cf26ba8cab1
Jared S <jaredjames2020@gmail.com>
2019-10-16 18:25:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -31,13 +31,13 @@ class Triangle
     #   rescue TriangleError => error
     #       puts error.message
     
-    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
-      begin
-          raise TriangleError
-        rescue TriangleError => error
-          puts error.message
-      end
-    end
+    # if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+    #   begin
+    #       raise TriangleError
+    #     rescue TriangleError => error
+    #       puts error.message
+    #   end
+    # end
     
   end
   
