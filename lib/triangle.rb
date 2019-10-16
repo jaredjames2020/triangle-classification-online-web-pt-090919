c3db87ff1d5685d9392c22e9617c8d68b7ebc497
Jared S <jaredjames2020@gmail.com>
2019-10-16 18:15:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,8 +28,8 @@ class Triangle
     #   rescue TriangleError => error
     #       puts error.message
     elsif @side1 == 0 || @side2 == 0 || @side3 == 0
-      # begin
-           raise TriangleError
+       begin
+           #raise TriangleError
         # rescue TriangleError => error
         #     puts error.message
     #end
