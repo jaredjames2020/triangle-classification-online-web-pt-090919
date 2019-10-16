73522d7dbeab0ede46d6e34b6aec1f6130364766
Jared S <jaredjames2020@gmail.com>
2019-10-16 18:25:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,6 +17,15 @@ class Triangle
   end
   
   def kind
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      begin
+          raise TriangleError
+        rescue TriangleError => error
+          puts error.message
+      end
+    end
+    
+    
     if @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif @side1 == @side3 || @side1 == @side2 || @side2 == @side3
@@ -31,13 +40,6 @@ class Triangle
     #   rescue TriangleError => error
     #       puts error.message
     
-    # if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
-    #   begin
-    #       raise TriangleError
-    #     rescue TriangleError => error
-    #       puts error.message
-    #   end
-    # end
     
   end
   
