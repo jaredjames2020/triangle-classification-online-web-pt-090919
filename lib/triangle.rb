e40fbf3683cc76e96eb913c41606c7784bdc9269
Jared S <jaredjames2020@gmail.com>
2019-10-16 18:39:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,7 @@ class Triangle
       # end
     end
     
-    unless (@side1 + @side2) > @side3 || (@side2 + @side3) > @side1 || (@side3 + @side1) > @side2
+    unless (@side1 + @side2) > @side3 && (@side2 + @side3) > @side1 && (@side3 + @side1) > @side2
       # begin
         raise TriangleError
       # rescue TriangleError => error
