f753b31e826f81d3252089b4f09f102190d80115
Jared S <jaredjames2020@gmail.com>
2019-10-16 17:20:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,14 @@
 class Triangle
   # write code here
   
-  def initialize()
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
   
+  def kind
+  end
   
   
 end
