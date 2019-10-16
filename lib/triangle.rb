02babcf767dd4dd72c7ee7e5db74fc90f4d3cf1b
Jared S <jaredjames2020@gmail.com>
2019-10-16 18:22:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,20 +19,22 @@ class Triangle
       :equilateral
     elsif @side1 == @side3 || @side1 == @side2 || @side2 == @side3
       :isosceles
-    elsif @side1 != @side3 || @side1 != @side2 || @side2 != @side3
+    else @side1 != @side3 || @side1 != @side2 || @side2 != @side3
       :scalene
+    end
+    
     # elsif (@side1 + @side3) < @side2 || (@side1 + @side2) < @side3 || (@side2 + @side3) < @side1
     #   begin
     #     raise TriangleError
     #   rescue TriangleError => error
     #       puts error.message
-    elsif @side1 <= 0 || @side2 <= 0 || @side3 <= 0
-      # begin
-           raise TriangleError
-        # rescue 
-        #     puts "error.message"
-    # end
-  end
+    
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      begin
+          raise TriangleError
+        rescue TriangleError => error
+          puts error.message
+    end
     
 end
 end
